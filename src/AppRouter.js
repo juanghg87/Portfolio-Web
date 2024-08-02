1440f713d911a8415458bf179284e2def6f94134
Juan Guillermo Hernández <juanghg@gmail.com>
2024-08-01 21:34:52 -0500

    using react router dom

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import App from './App';
 import External1 from './external/External1';
 import External2 from './external/External2';
@@ -10,9 +11,10 @@ const AppRouter = () => {
     return (
         <Router basename="/Portfolio-Web/build">
             <Routes>
-                <Route path="/spa" element={<App/>} />
-                <Route path="/external1" element={<External1/>} />
-                <Route path="/external2" element={<External2/>} />
+                <Route path="/" element={<Navigate to="/spa" replace />} />
+                <Route path="/spa" element={<App />} />
+                <Route path="/external1" element={<External1 />} />
+                <Route path="/external2" element={<External2 />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
